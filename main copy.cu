792753570af3e2a7ffee0ec0975d1190309e63ca
cguzman95 <christian.guzman@bsc.es>
2026-05-03 14:25:39 +0200

    flattened 1D C

--- main copy.cu
+++ main copy.cu
@@ -5,14 +5,19 @@
 
 #define T_BLOCK 1024
 
-static int min3(int a, int b, int c) {
+__device__ static int dmin3(int a, int b, int c) {
   int m = a < b ? a : b;
   return m < c ? m : c;
 }
 
-__device__ static int dmin3(int a, int b, int c) {
-  int m = a < b ? a : b;
-  return m < c ? m : c;
+__global__ void d_init_dpi(int *dp, int m) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  dp[i * m] = i;
+}
+
+__global__ void d_init_dpj(int *dp) {
+  int j = blockIdx.x * blockDim.x + threadIdx.x;
+  dp[j] = j;
 }
 
 __global__ void d_edit_core(const char *a, const char *b, int *dp, int n,
@@ -34,43 +39,45 @@ int edit_distance(const char *a, const char *b) {
   int n = (int)strlen(a) + 1;
   int m = (int)strlen(b) + 1;
 
+  int **dp = (int **)malloc(n * sizeof(int *));
+  for (int i = 0; i < n; i++) {
+    dp[i] = (int *)malloc(m * sizeof(int));
+  }
+
   int *ddp;
   const char *da, *db;
   cudaMalloc(&ddp, n * m * sizeof(int));
+  cudaMemset(ddp, 0, n * m * sizeof(int));
   cudaMalloc(&da, n * sizeof(char));
   cudaMalloc(&db, m * sizeof(char));
 
-  int *dp = (int *)malloc(n * m * sizeof(int));
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      dp[i * m + j] = 0;
-    }
-  }
+  /*
+  int blocks;
+  blocks = (n + T_BLOCK - 1) / T_BLOCK;
+  d_init_dpi<<<blocks, T_BLOCK>>>(ddp, m);
+  blocks = (m + T_BLOCK - 1) / T_BLOCK;
+  d_init_dpj<<<blocks, T_BLOCK>>>(ddp);
+*/
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      dp[i][j] = 0;
 
   for (int i = 0; i < n; i++)
-    dp[i * m] = i;
+    dp[i][0] = i;
   for (int j = 0; j < m; j++)
-    dp[j] = j;
-  int result;
+    dp[0][j] = j;
 
-  /*
-  cudaMemcpy(ddp, dp, n * m * sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(ddp, *dp, n * m * sizeof(int), cudaMemcpyHostToDevice);
+
   cudaDeviceSynchronize();
   d_edit_core<<<1, 1>>>(da, db, ddp, n, m);
+
+  int result;
   cudaMemcpy(&result, &ddp[(n - 1) * m + (m - 1)], sizeof(int),
              cudaMemcpyDeviceToHost);
-*/
 
-  for (int i = 1; i < n; i++) {
-    for (int j = 1; j < m; j++) {
-      int cost = (a[i - 1] == b[j - 1]) ? 0 : 1;
-      dp[i * m + j] = min3(dp[(i - 1) * m + j] + 1, dp[i * m + (j - 1)] + 1,
-                           dp[(i - 1) * m + (j - 1)] + cost);
-    }
-  }
-
-  result = dp[(n - 1) * m + (m - 1)];
-
+  for (int i = 0; i < n; i++)
+    free(dp[i]);
   free(dp);
   return result;
 }
