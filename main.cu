4495913489ee9887663736cd0f20ae3eca6b1acd
cguzman95 <christian.guzman@bsc.es>
2026-05-03 14:39:37 +0200

    added init in gpu

--- main.cu
+++ main.cu
@@ -5,14 +5,21 @@
 
 #define T_BLOCK 1024
 
-static int min3(int a, int b, int c) {
+__device__ static int dmin3(int a, int b, int c) {
   int m = a < b ? a : b;
   return m < c ? m : c;
 }
 
-__device__ static int dmin3(int a, int b, int c) {
-  int m = a < b ? a : b;
-  return m < c ? m : c;
+__global__ void d_init_dpi(int *dp, int m, int n) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < n)
+    dp[i * m] = i;
+}
+
+__global__ void d_init_dpj(int *dp, int m) {
+  int j = blockIdx.x * blockDim.x + threadIdx.x;
+  if (j < m)
+    dp[j] = j;
 }
 
 __global__ void d_edit_core(char *a, char *b, int *dp, int n, int m) {
@@ -35,28 +42,22 @@ int edit_distance(const char *a, const char *b) {
 
   int *ddp;
   char *da, *db;
-  cudaMalloc(&ddp, n * m * sizeof(int));
   cudaMalloc(&da, n * sizeof(char));
   cudaMalloc(&db, m * sizeof(char));
   cudaMemcpy(da, a, n * sizeof(char), cudaMemcpyHostToDevice);
   cudaMemcpy(db, b, m * sizeof(char), cudaMemcpyHostToDevice);
+  cudaMalloc(&ddp, n * m * sizeof(int));
+  cudaMemset(ddp, 0, n * m * sizeof(int));
 
-  int *dp = (int *)malloc(n * m * sizeof(int));
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      dp[i * m + j] = 0;
-    }
-  }
+  int blocks = (n + T_BLOCK - 1) / T_BLOCK;
+  d_init_dpi<<<blocks, T_BLOCK>>>(ddp, m, n);
+  cudaDeviceSynchronize();
+  blocks = (m + T_BLOCK - 1) / T_BLOCK;
+  d_init_dpj<<<blocks, T_BLOCK>>>(ddp, m);
 
-  for (int i = 0; i < n; i++)
-    dp[i * m] = i;
-  for (int j = 0; j < m; j++)
-    dp[j] = j;
-  int result;
-
-  cudaMemcpy(ddp, dp, n * m * sizeof(int), cudaMemcpyHostToDevice);
   cudaDeviceSynchronize();
   d_edit_core<<<1, 1>>>(da, db, ddp, n, m);
+  int result;
   cudaMemcpy(&result, &ddp[(n - 1) * m + (m - 1)], sizeof(int),
              cudaMemcpyDeviceToHost);
 
@@ -69,9 +70,9 @@ int edit_distance(const char *a, const char *b) {
     }
   }
   result = dp[(n - 1) * m + (m - 1)];
+  free(dp);
 */
 
-  free(dp);
   return result;
 }
 
